982e04c1786c6cc3f42f87c16f99bcbc3292ea99
Eduardo Olivo <keolivor@utn.edu.ec>
2021-07-18 21:50:17 -0500

    Se crearon todos los metodos en la clase SubirLibroManager

---
+++ src/modelos/entidades/LibroModel.java
@@ -0,0 +1,63 @@
+
+package modelos.entidades;
+
+public class LibroModel {
+    
+    private int id_libro;
+    private String titulo;
+    private String descripcion;
+    private String autor;
+    private String url;
+    private boolean estado;
+
+    public int getId_libro() {
+        return id_libro;
+    }
+
+    public void setId_libro(int id_libro) {
+        this.id_libro = id_libro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
